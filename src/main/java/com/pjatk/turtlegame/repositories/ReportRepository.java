e43abdf0c070701f3e8f6da7716d45db2e0342ea
Aleksandra Wierzbicka <a.wierzbicka0579@gmail.com>
2023-07-08 14:54:34 +0200

    Dodano szablon do widoku strony

--- src/main/java/com/pjatk/turtlegame/repositories/ReportRepository.java
+++ src/main/java/com/pjatk/turtlegame/repositories/ReportRepository.java
@@ -1,7 +1,6 @@
 package com.pjatk.turtlegame.repositories;
 
 import com.pjatk.turtlegame.models.Report;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
