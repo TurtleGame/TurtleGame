c50d0cc0426e09e0645bce8997220ba5c314ac47
Wicha <s22474@pjwstk.edu.pl>
2023-09-27 14:44:22 +0200

    ZMIANY: - Dodano walidację dla formularza nadania jajku imienia - Dodano limit na liczbę jaj w inkubatorze

--- src/main/java/com/pjatk/turtlegame/controllers/NestController.java
+++ src/main/java/com/pjatk/turtlegame/controllers/NestController.java
@@ -1,6 +1,10 @@
 package com.pjatk.turtlegame.controllers;
 
 import com.pjatk.turtlegame.config.TurtleUserDetails;
+import com.pjatk.turtlegame.exceptions.TurtleNotFoundException;
+import com.pjatk.turtlegame.exceptions.UnauthorizedAccessException;
+import com.pjatk.turtlegame.models.DTOs.EggsForm;
+import com.pjatk.turtlegame.models.DTOs.FeedTurtleDTO;
 import com.pjatk.turtlegame.repositories.UserRepository;
 import com.pjatk.turtlegame.services.ItemService;
 import com.pjatk.turtlegame.services.TurtleEggService;
@@ -10,6 +14,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -26,6 +31,7 @@ public class NestController {
     public String index(Model model, @AuthenticationPrincipal TurtleUserDetails turtleUserDetails) {
         model.addAttribute("context", "nest");
         model.addAttribute("eggs", itemService.getEggs(turtleUserDetails.getId()));
+        model.addAttribute("eggsForm", new EggsForm());
 
         return "pages/nest";
     }
@@ -37,7 +43,23 @@ public class NestController {
     }
 
     @PostMapping("/{id}/adopt")
-    public String adoptEgg(@AuthenticationPrincipal TurtleUserDetails turtleUserDetails, @PathVariable int id, @RequestParam("Name") String name) throws Exception {
+    public String adoptEgg(@ModelAttribute("eggsForm") EggsForm eggsForm,
+                           @AuthenticationPrincipal TurtleUserDetails turtleUserDetails,
+                           @PathVariable int id,
+                           @RequestParam("Name") String name,
+                           Model model,
+                           BindingResult bindingResult) throws Exception {
+        //TODO
+        if (name.length() < 2 || name.length() > 50) {
+            bindingResult.rejectValue("Name", "error.wrongName", "Nieprawidłowe imię");
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("eggs", itemService.getEggs(turtleUserDetails.getId()));
+
+            return "pages/nest";
+        }
+
         itemService.adoptEgg(turtleUserDetails.getId(), id, name);
         return "redirect:/nest";
     }
---
+++ src/main/java/com/pjatk/turtlegame/models/DTOs/EggsForm.java
@@ -0,0 +1,16 @@
+package com.pjatk.turtlegame.models.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EggsForm {
+
+    private Integer eggId;
+    private String name;
+}
