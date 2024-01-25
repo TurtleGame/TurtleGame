7d1bfa7c9fd5b9b31d24e9e2675392a7c089ba67
Aleksandra Wierzbicka <a.wierzbicka0579@gmail.com>
2024-01-25 15:52:18 +0100

    ZMIANY: - dodanie testów integracyjnych dla controllera AchievementsController

---
+++ src/test/java/com/pjatk/turtlegame/AchievementsControllerTest.java
@@ -0,0 +1,43 @@
+package com.pjatk.turtlegame;
+
+import com.pjatk.turtlegame.config.TurtleUserDetails;
+import com.pjatk.turtlegame.models.Turtle;
+import com.pjatk.turtlegame.models.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AchievementsControllerTest extends BaseTest{
+
+    protected TurtleUserDetails firstUserDetails;
+
+    @BeforeAll
+    public void setup(){
+        User user1 = makeUser("1AchievTester", true);
+
+        firstUserDetails = new TurtleUserDetails(user1);
+
+    }
+
+    @Test
+    void canSeeAchievementsPage() throws Exception {
+        MockHttpServletRequestBuilder request = get("/achievements").with(user(firstUserDetails));
+        MvcResult result = mockMvc.perform(request).andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        assertThat(result.getResponse().getContentAsString()).contains("Osiagnięcia");
+    }
+}
