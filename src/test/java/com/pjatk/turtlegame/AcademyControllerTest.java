478712ae07b7e4d857ab2d6dcfa8d7a81a8aefff
Aleksandra Wierzbicka <a.wierzbicka0579@gmail.com>
2024-01-25 15:57:34 +0100

    ZMIANY: - dodanie testów integracyjnych dla controllera AcademyController

---
+++ src/test/java/com/pjatk/turtlegame/AcademyControllerTest.java
@@ -0,0 +1,56 @@
+package com.pjatk.turtlegame;
+
+import com.pjatk.turtlegame.config.TurtleUserDetails;
+import com.pjatk.turtlegame.models.Turtle;
+import com.pjatk.turtlegame.models.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AcademyControllerTest extends BaseTest {
+
+    protected TurtleUserDetails firstUserDetails;
+
+    protected TurtleUserDetails secondUserDetails;
+
+    @BeforeAll
+    public void setup() {
+        User user1 = makeUser("1UserTurtle", true);
+
+        firstUserDetails = new TurtleUserDetails(user1);
+    }
+
+    @Test
+    void canSeeAcademyPage() throws Exception {
+        MockHttpServletRequestBuilder request = get("/academy").with(user(firstUserDetails));
+        MvcResult result = mockMvc.perform(request).andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+        assertThat(result.getResponse().getContentAsString()).contains("Akademia");
+    }
+
+    @Test
+    void seeIfCanTraining() throws Exception {
+
+        MockHttpServletRequestBuilder request = get("/academy/if-training-can?trainingId=1&duration=180").with(user(firstUserDetails));
+
+        MvcResult result = mockMvc.perform(request).andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).isEqualTo("false");
+
+    }
+
+}
